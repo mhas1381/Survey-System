85d36b8595b5750114428e9002836d97bd83a18a
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-12 18:30:54 +0430

    start app website

--- Survey/urls.py
+++ Survey/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',hello)
+    path('' , include('website.urls'))
 ]
