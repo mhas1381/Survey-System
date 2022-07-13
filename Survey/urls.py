2a49eb3cc836617d463d88dfb0f1265264e07aab
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-13 12:02:06 +0430

    redirect to admin page

--- Survey/urls.py
+++ Survey/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from website.urls import *
 from accounts.urls import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,name='test'),
     path('' , include('website.urls')),
     path('accounts/', include('accounts.urls')),
 ]
