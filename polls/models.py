092e7086ced115f96e76dd164ae81a05db2abd5d
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-15 12:19:02 +0430

    creating database

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.question
+
+class Choise(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE,related_name='choises')
+    option = models.CharField(max_length=100)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.option
+        
